a59706425bbcc8b6292b5e9e54bca78913d14b54
redducc <sushmit718@gmail.com>
2024-02-08 18:21:59 +0100

    sky: Inherit 4gb dalvik heap instead of 6gb
    Co-authored-by: jendermine <jendermine@gmail.com>

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Setup dalvik vm configs.
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Call the proprietary setup.
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
