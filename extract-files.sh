StatiXOS/android_device_xiaomi_sky

sky: Use libhidlbase_shim for vendor.libdpmframework
Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce

extract-files.sh
@@ -85,6 +85,10 @@ function blob_fixup() {
             sed -i '/<!-- Remote Submix Audio HAL -->/i <!-- Bluetooth Audio HAL -->\n<xi:include href="/vendor/etc/bluetooth_audio_policy_configuration.xml"/>' "${2}"
             sed -i -e '/<!-- Bluetooth Audio HAL for hearing aid -->/d' -e '/<xi:include href="\/vendor\/etc\/bluetooth_qti_hearing_aid_audio_policy_configuration.xml"\/>/d' "${2}"
             ;;
+        vendor/lib64/vendor.libdpmframework.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
     esac
 }
 

dhanush281/pp

extract-files.sh
@@ -131,6 +131,9 @@ function blob_fixup() {
         vendor/lib64/libqtikeymint.so)
             "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
             ;;
+        vendor/lib64/vendor.libdpmframework.so)
+            "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
     esac
 }
 
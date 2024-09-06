7adb2136b5b3178b6bfd0d76a75d8047fd1ec9ad
Arian <arian.kulmer@web.de>
2024-09-06 14:01:17 +0200

    sky: Use libhidlbase_shim for vendor.libdpmframework
    Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce

--- extract-files.sh
+++ extract-files.sh
@@ -131,6 +131,9 @@ function blob_fixup() {
         vendor/lib64/libqtikeymint.so)
             "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
             ;;
+        vendor/lib64/vendor.libdpmframework.so)
+            "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
     esac
 }
 
